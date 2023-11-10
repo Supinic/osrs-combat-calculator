4657f253ad0ea944c561fe182eeb5b96d976f4ca
Supinic <supinic@protonmail.com>
2023-11-10 17:37:16 +0100

    added accuracy and max-hit general formulas

--- src/utils.ts
+++ src/utils.ts
@@ -3,3 +3,11 @@ type Entries<T> = { [K in keyof T]: [K, T[K]] }[keyof T];
 export function typeEntries<T extends object>(t: T): Entries<T>[] {
     return Object.entries(t) as Entries<T>[];
 }
+
+export function generalAccuracyFormula (a: number, b: number) {
+    return a * (b + 64);
+}
+
+export function generalMaxHitFormula (a: number, b: number) {
+    return Math.floor(0.5 + a * (b + 64) / 640);
+}
